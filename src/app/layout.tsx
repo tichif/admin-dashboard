75c6f75215fad36364023bc0477863628b1258d2
Dalzon Charles-Hébert <charlby5@gmail.com>
2024-06-03 21:42:15 -0400

    Create the Sidebar menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 
 import Navbar from '@/components/Navbar';
+import Sidebar from '@/components/Sidebar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,7 +21,12 @@ export default function RootLayout({
     <html lang='en'>
       <body className={inter.className}>
         <Navbar />
-        {children}
+        <div className='flex'>
+          <div className='h-[100vh] hidden md:block w-[300px]'>
+            <Sidebar />
+          </div>
+          <div className='p-5 w-full md:max-w-[1140px]'> {children}</div>
+        </div>
       </body>
     </html>
   );
